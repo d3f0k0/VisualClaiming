fa1626b102916a33708d1a040916ff996a4392e7
Trần Nguyên Bình <tnguyenbinhxyz@gmail.com>
2025-07-09 09:24:46 +0700

    Made it client side only

--- src/main/java/com/alchemy/visualclaiming/VisualClaiming.java
+++ src/main/java/com/alchemy/visualclaiming/VisualClaiming.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]", clientSideOnly = true)
 public class VisualClaiming {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MODID);
